aed2507a0533d0665eb77f349dfeb40402ecb171
Renato Araujo da Silva <97402553+coldrenatinho@users.noreply.github.com>
2024-03-02 22:27:17 -0400

    ez

---
+++ 2024-02-26/Ex013.c
@@ -0,0 +1,12 @@
+/*13) Escreva um programa para imprimir as letras de A a Z.*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    for (int i = 65; i <= 90; ++i)
+    {
+        printf("%c - ", i);
+    }
+}
